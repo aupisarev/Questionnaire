cb7e782e700bd3a75825d27fb42766001de588ba
Антон <aupisarev@gmail.com>
2021-03-15 12:33:48 +0900

    Create project

---
+++ Questionnaire/Questionnaire.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Questionnaire.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Questionnaire/Questionnaire.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Questionnaire.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
